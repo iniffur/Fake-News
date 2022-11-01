343d9d3e3f537c82bec791306dfdef0b24960352
iniffur <jamesruffini97@gmail.com>
2022-11-01 14:33:54 +0000

    Add Cypress currently incomplete

---
+++ cypress/integration/check_home_spec.ts
@@ -0,0 +1,8 @@
+describe("Home page", () => {
+    it("has a title", () => {
+      cy.visit("/");
+    //   cy.get("title").should("contain", "login");
+      cy.contains("p", "Test");
+    });
+  });
+  
