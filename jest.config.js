fb1bb3f04b15d7e89c497e0776cdb97271655513
D. Dramchev <ddramchev@gmail.com>
2022-11-01 18:27:01 +0000

    Add cypress, needs configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+// };
+
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
