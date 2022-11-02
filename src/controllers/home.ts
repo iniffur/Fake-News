dfbeca5f94c4d15205d3815e426b2011765fa556
iniffur <jamesruffini97@gmail.com>
2022-11-02 16:14:30 +0000

    IFrame redirects result page onto index

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -2,26 +2,13 @@ import { Request, Response } from "express";
 import Checker from "../model/home";
 
 const HomeController = {
-  // Index: (req: Request, res: Response) => {
-  //   res.render("home/index", { title: "This Reeks" });
-  // },
+  Index: (req: Request, res: Response) => {
+    res.render("home/index", { title: "This Reeks" });
+  },
   Check: (req: Request, res: Response) => {
-    // const submitButton = document.getElementById("submitButton");
-    // const resultDiv = document.getElementById("resultDiv");
-
-    const submitButton = req.body.submitButton;
-    const resultDiv = req.body.resultDiv;
-
-    const text = "req.body.headline;";
+    const text = req.body.headline;
     const outputString = new Checker(text).check();
 
-    submitButton.addEventListener("click", function handleClick() {
-      console.log("clicked button");
-      resultDiv.collapse.show;
-    });
-    // function handleClick(this: resultDiv) {
-
-    // }
     console.log("displaying");
     res.render("home/index", {
       result: outputString,
@@ -29,15 +16,6 @@ const HomeController = {
       title: "This Reeks",
     });
   },
-  // CallChecker: (req: Request, res: Response) => {
-  //   const text = req.body.headline;
-  //   const checkerFunction = new Checker(text);
-  //   const outputArray = checkerFunction.check();
-  //   const outputString =
-  //     outputArray[0] <= 20
-  //       ? `${outputArray[2]}Somewhat believable.${outputArray[1]}`
-  //       : `${outputArray[2]}Trash.${outputArray[1]}`;
-  // },
 };
 
 export default HomeController;
--- src/routes/home.ts
+++ src/routes/home.ts
@@ -2,7 +2,8 @@ import express from "express";
 const router = express.Router();
 import HomeController from "../controllers/home";
 
-router.get("/", HomeController.Check);
-// router.post("/", HomeController.Check);
+router.get("/", HomeController.Index);
+router.post("/", HomeController.Check);
+router.get("/result", HomeController.Check);
 
 export default router;
