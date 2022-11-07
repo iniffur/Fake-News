b07018a04b4d547df15acd83aec008669a955ead
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-11-07 15:53:40 +0000

    Newsfeed ordered based on bullshit percentage

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -10,7 +10,12 @@ dotenv.config();
 
 const HomeController = {
   Index: async (req: Request, res: Response) => {
-    const newsHeadlines = new NewsFormatter().outputNews();
+    let newsHeadlines = new NewsFormatter().outputNews();
+
+    newsHeadlines = newsHeadlines.sort((a: any, b: any) =>
+      (b.percentage < a.percentage )? 1 : (a.percentage < b.percentage) ? -1 : 0
+    ).reverse();
+    console.log(newsHeadlines);
     const topTen = newsHeadlines.slice(0, 10);
 
     res.render("home/index", {
@@ -19,14 +24,16 @@ const HomeController = {
     });
   },
   Check: async (req: Request, res: Response) => {
+    let googleApiStatement = null;
+    let googleApiResults = null;
     const inputText = req.body.headline;
     const outputString = await new Checker(inputText).check();
-    const googleApiStatement =
-      await new GoogleFormatter().outputGoogleStatements(inputText);
-    const googleApiResults = await new GoogleFormatter().outputGoogleResults(
+    googleApiStatement = await new GoogleFormatter().outputGoogleStatements(
       inputText
     );
-
+    googleApiResults = await new GoogleFormatter().outputGoogleResults(
+      inputText
+    );
     res.render("home/result", {
       result: outputString,
       googleContent: googleApiStatement,
