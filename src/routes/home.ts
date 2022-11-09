778f6c3e2039d2363aea173c8a664ca6488b9534
iniffur <jamesruffini97@gmail.com>
2022-11-09 17:27:05 +0000

    Attempting IFrame fix

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 import HomeController from "../controllers/home";
 
 router.get("/", HomeController.Index);
-router.get("/GBHeadlines", HomeController.Index);
+// router.get("/GBHeadlines", HomeController.Index);
 router.post("/", HomeController.Check);
 router.post("/GBHeadlines", HomeController.GBHeadlines);
 router.post("/USHeadlines", HomeController.USHeadlines);
